a78cdc8984fcedd9771a3ac62d9e2ffaadf08c55
hongyu.li <hongyu.li@thoughtworks.com>
2019-10-10 19:16:44 +0800

    complete calculator by using mobx

--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import './index.css';
+import { observer, inject } from 'mobx-react';
 
+@inject('store')
+@observer
 class Calculator extends Component {
   render(){
-    const operations = ['+','-','x','1','2','3','4','5','6','7','8','9','0','clear','='];
+    const operations = ['+','-','*','1','2','3','4','5','6','7','8','9','0','clear','='];
+    const {store} = this.props;
     return (
     <div className= "calculator">
-        <span className='calculation'></span>
+        <span className='calculation'>{store.answer}</span>
         <div className='operation-board'>
-            {operations.map((item)=>
-                <button id={item} className='operations'>{item}</button>
+            {operations.map((item,i)=>
+                <button id={i} className='operations' onClick={()=>store.calculate(item)}>{item}</button>
             )}
         </div>
    </div>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,29 @@
 import {observable, action} from 'mobx';
 
 class AppStore {
-    @observable number = 0;
+    @observable calculation = '';
+    @observable answer = '';
 
-    @action.bound addNumber() {
-        this.number++;
+    @action.bound calculate(operation) {
+        switch(operation) {
+            case 'clear':
+                this.calculation = '';
+                this.answer = 0;
+                break;
+            case '=':
+                try {
+                    this.answer = eval(this.calculation);
+                }catch {
+                    window.alert('请输入正确的计算公式！');
+                    this.answer = 0
+                }finally{
+                    this.calculation = '';
+                }
+                break;
+            default:
+                this.calculation = this.calculation + operation;
+                this.answer = this.calculation
+        }        
     }
 };
 
