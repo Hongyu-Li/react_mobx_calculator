84bd1d5f265d02b17eedbae2c1ac8b1f4ad348a7
hongyu.li <hongyu.li@thoughtworks.com>
2019-10-10 19:19:46 +0800

    change the project to be completed

--- src/Header/index.js
+++ src/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
     <div className= "header">
         <ul>
             <li><img alt="avatar" src="./avatar.png" width="50px" height='50px'/></li>
-            <li><span className="dot"></span>Done</li>
+            <li><span className="dot"></span>Completed</li>
             <li><span><a href='https://github.com/Hongyu-Li/react_mobx_calculator/'>Get in Touch</a></span></li>
         </ul>
    </div>
